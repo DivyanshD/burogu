e84423c14b75a225bc9dac4ea55ff427c07b734d
saurabhraturi2000 <saurabh.raturi.999@gmail.com>
2023-04-04 11:10:56 +0530

    daisy ui installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignupPage from "./pages/Signup";
 import LoginPage from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <BrowserRouter>
+        <Router>
           <Routes>
-            <Route path="/" element={<LoginPage />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </div>
     </div>
   );
