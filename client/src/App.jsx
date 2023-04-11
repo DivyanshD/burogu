500883b6b1d091bad8958b137150d46a11f8c7e3
saurabhraturi2000 <saurabh.raturi.999@gmail.com>
2023-04-11 10:54:48 +0530

    google auth done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,58 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
+
 import SignupPage from "./pages/Signup";
 import LoginPage from "./pages/Login";
 import Home from "./pages/Home";
+import WriteBlog from "./pages/WriteBlog";
 
 function App() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const [blog, setBlog] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/blogs/")
+      .then(function (response) {
+        // handle success
+        // console.log(response);
+        setBlog(response.data.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
   return (
-    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signin" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-          </Routes>
-        </Router>
-      </div>
-    </div>
+    <GoogleOAuthProvider clientId="client id">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              user === null ? (
+                <Navigate to="/signin" replace />
+              ) : (
+                <Home user={user.data} userObj={user} blog={blog} />
+              )
+            }
+          />
+          <Route path="/signin" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/writeblog" element={<WriteBlog />} />
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
